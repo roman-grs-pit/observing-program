d1758c09dce875a89e5cd6c8b7a4111200d2e08e
ashleyjross <ashley.jacob.ross@gmail.com>
2025-07-10 11:25:34 -0400

    Update get_8exp_coverage.py

--- scripts/get_8exp_coverage.py
+++ scripts/get_8exp_coverage.py
@@ -34,6 +34,8 @@ import footprintutils as fp
 import optical_model
 optmod = optical_model.RomanOpticalModel()
 
+
+
 #should move this to code library
 def test_foot(xpix,ypix,min_pix=0,max_pix=4088,det=1,min_lam_4foot=1.6,max_lam_4foot=1.93):
     #xpix,ypix is the pixel center for the object in the direct image
@@ -68,6 +70,13 @@ def mkgrid(ra,dec,sz,res):
             ral_tot.append(ral[i])
             decl_tot.append(decl[j])
     return ral_tot,decl_tot
+
+def cutran(ramin,ramax,decmin,decmax):
+    sel = ral > ramin
+    sel &= ral < ramax
+    sel &= decl > decmin
+    sel &= decl < decmax
+    return ral[sel],decl[sel]
 
 def get_pixl(coords,detfoot,detnum,PA):
     #coords are ra,dec points to test in astropy format
@@ -110,11 +119,9 @@ def plot_dets_rsiaf(att_in,ax):
         ax.plot(corners_ra,corners_dec,'k')
 
 
-ral_tot,decl_tot = mkgrid(0,0,1,100)
-coords = SkyCoord(ra=ral_tot*u.degree,dec=decl_tot*u.degree, frame='icrs')
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--ra0", help="ra center",default=0)
+parser.add_argument("--ra0", help="ra center",default=2)
+parser.add_argument("--randens", help="density of random points /deg2 to use",default=2500)
 parser.add_argument("--dec0", help="dec center",default=0)
 parser.add_argument("--dithra", help="dither step in the ra direction",default=0.025)
 parser.add_argument("--dithdec", help="dither step in the dec direction",default=0.05)
@@ -126,6 +133,23 @@ args = parser.parse_args()
 
 ra0 = args.ra0
 dec0 = args.dec0
+
+decm = dec0-0.6
+decx = deco+0.6
+ram = ra0-0.4
+rax = ra0+0.4
+randens = args.randens
+fullsky_area = 360.*360/np.pi
+nran = randens*fullsky_area
+acosl = np.random.rand(nran)*2-1 #distribute randomly in arccos
+ral = np.random.rand(nran)*360-180
+decl = np.arccos(acosl)
+
+ral_tot,decl_tot = cutran(ram,rax,decm,decx)#mkgrid(0,0,1,100)
+print(str(len(ral_tot))+' random points will be used')
+coords = SkyCoord(ra=ral_tot*u.degree,dec=decl_tot*u.degree, frame='icrs')
+
+
 dithstep = args.dithra,args.dithdec
 ndith = 2
 decpa = args.pa_step_dec
@@ -149,8 +173,8 @@ raoff = raoffl[0]
 #a = rctrans.wfi_sky_pointing(ra0+raoff, dec0+decoff, pa, ds9=False,ax=ax)#+dith*dithstep[0]
 att = attitude(wfi_cen.V2Ref, wfi_cen.V3Ref, ra0+raoff, dec0+decoff, pa)
 plot_dets_rsiaf(att,ax)
-ax.set_xlim(-.6, 0.6)
-ax.set_ylim(-0.4,.4)
+ax.set_xlim(ram, rax)
+ax.set_ylim(decm,decx)
 plt.grid()
 plt.title('1 exposure')
 plt.savefig(outdir+'oneexp.png')
@@ -168,8 +192,8 @@ for dith in range(0,ndith):
     #a = rctrans.wfi_sky_pointing(ra0+raoff+dith*dithstep[0], dec0+decoff+dith*dithstep[1], pa, ds9=False,ax=ax)#+dith*dithstep[0]
     att = attitude(wfi_cen.V2Ref, wfi_cen.V3Ref, ra0+raoff+dith*dithstep[0], dec0+decoff+dith*dithstep[1], pa)
     plot_dets_rsiaf(att,ax)
-ax.set_xlim(-.6, 0.6)
-ax.set_ylim(-0.4,.4)
+ax.set_xlim(ram, rax)
+ax.set_ylim(decm,decx)
 plt.title('2 dithers')
 plt.savefig(outdir+'twodithers.png')
 plt.show()
@@ -184,8 +208,8 @@ for pa,decoff,raoff in zip(pal,decoffl,raoffl):
     att = attitude(wfi_cen.V2Ref, wfi_cen.V3Ref, ra0+raoff, dec0+decoff, pa)
     plot_dets_rsiaf(att,ax)
 
-ax.set_xlim(-.6, 0.6)
-ax.set_ylim(-0.4,.4)
+ax.set_xlim(ram, rax)
+ax.set_ylim(decm,decx)
 plt.title('4 rolls')
 plt.grid()
 plt.savefig(outdir+'fourrolls.png')
@@ -203,8 +227,8 @@ for pa,decoff,raoff in zip(pal,decoffl,raoffl):
         att = attitude(wfi_cen.V2Ref, wfi_cen.V3Ref, ra0+raoff+dith*dithstep[0], dec0+decoff+dith*dithstep[1], pa)
         plot_dets_rsiaf(att,ax)
 
-ax.set_xlim(-.6, 0.6)
-ax.set_ylim(-0.4,.4)
+ax.set_xlim(ram, rax)
+ax.set_ylim(decm,decx)
 plt.title('all 8 exposures')
 plt.savefig(outdir+'all8exp.png')
 plt.show()
@@ -247,8 +271,8 @@ sel = nobs > 0
 plt.scatter(np.array(ral_tot)[sel],np.array(decl_tot)[sel],c=nobs[sel],s=.1,cmap=cmap,vmin=0.5,vmax=8.5)
 plt.colorbar(ticks=np.arange(1,9 ),label='# of obs.')
 #plt.colorbar(levels=[0,1,2,3,4])
-plt.xlim(min(ral_tot),max(ral_tot))
-plt.ylim(min(decl_tot),max(decl_tot))
+plt.xlim(ram, rax)
+plt.ylim(decm,decx)
 plt.xlabel('ra (degrees)')
 plt.ylabel('dec (degrees)')
 plt.title(r'footprint 4 rolls/2 dithers')
