0b986181033716b6e98a73edf350f0b9d5b9bf21
ashleyjross <ashley.jacob.ross@gmail.com>
2026-05-08 11:58:47 -0400

    Update get_intile_coverage_inDESIran.py

--- scripts/get_intile_coverage_inDESIran.py
+++ scripts/get_intile_coverage_inDESIran.py
@@ -66,9 +66,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--wficen", help="if y, positions are detector center", default='y')
 parser.add_argument(
-    "--optmodver", help="version of optical model to use", default='v08')
+    "--optmodver", help="version of optical model to use", default='gdps', choices=['v06', 'v08', 'gdps'])
 parser.add_argument(
-    "--coords_ver", help="version of ra,dec -> fpa transformation to use", default='gdps', choices=['gdps', 'tan'])
+    "--coords_ver", help="version of ra,dec -> fpa transformation to use", default='tan', choices=['gdps', 'tan'])
 
 parser.add_argument(
     "--wavmin", help="set minimum wavelength, if not None", default=None)
@@ -77,22 +77,34 @@ parser.add_argument(
 parser.add_argument(
     "--chunksize", help="objects to process per chunk", default=10000000, type=int)
 parser.add_argument("--racol", help="column name for RA", default='RA')
-parser.add_argument("--deccol", help="column name for RA", default='DEC')
+parser.add_argument("--deccol", help="column name for DEC", default='DEC')
 parser.add_argument(
     "--IDcol", help="column name for unique ID", default='TARGETID')
 parser.add_argument("--tilefile", help="full path to tile file",
-                    default=os.environ['github_dir']+'observing-program/data/roman_hlwas.sim.ecsv')
+                    default=os.environ['github_dir']+'observing-program/data/994-hlwas-Feb26.sim.ecsv')
 parser.add_argument(
     "--nran", help="number of randoms to use", default=1, type=int)
 # parser.add_argument("--output", help="full path to output file",default=os.environ['SCRATCH']+'/test4deg2.fits')
 parser.add_argument(
     "--outroot", help="root directory for output", default=os.environ['SCRATCH'])
-parser.add_argument("--ramin", help="ra center", default=49, type=float)
-parser.add_argument("--ramax", help="ra center", default=51, type=float)
-parser.add_argument("--decmin", help="dec center", default=-11, type=float)
-parser.add_argument("--decmax", help="dec center", default=-9, type=float)
+parser.add_argument(
+    "--outname", help="additional name for output files", default='_test')
+parser.add_argument("--ramin", help="minimum ra", default=49, type=float)
+parser.add_argument("--ramax", help="maximum ra", default=51, type=float)
+parser.add_argument("--decmin", help="minimum dec", default=-11, type=float)
+parser.add_argument("--decmax", help="maximum dec", default=-9, type=float)
+parser.add_argument("--target", help="which targets to include based on TARGET_NAME in tile file (ignored if not in tile file), options are medium (default, includes all except XMM and COSMOS), xmm, cosmos",
+                    default='medium', choices=['medium', 'xmm', 'cosmos'])
 parser.add_argument(
     "--fullsurvey", help="If set, override any ra,dec bounds and simulate the whole survey", action='store_true')
+parser.add_argument(
+    "--palist", help="list of PA values for repeated RA,DEC", default=None, type=str, nargs='*')
+parser.add_argument(
+    "--radiff", help="diff in RA for the repeated values", default=0, type=float)
+parser.add_argument(
+    "--decdiff", help="diff in DEC for the repeated values", default=0, type=float)
+parser.add_argument(
+    "--decpa", help="add diff in DEC for the flipped roll angles", default=0, type=float)
 
 args = parser.parse_args()
 
@@ -166,17 +178,50 @@ if not os.path.exists(outdir):
     os.makedirs(outdir)
 
 fstr = 'DESIran'+str(args.nran)+'_lam'+str(minwav)+str(maxwav)
-outf = outdir+'nobs'+fstr+'grid_'+args.optmodver+'.ecsv'
+outf = outdir+'nobs'+fstr+'grid_'+args.optmodver+args.outname+'.ecsv'
 logger.info('will save results to '+outf)
 
 
 tiles = Table.read(args.tilefile)
 gtiles = tiles['BANDPASS'] == 'GRISM'
+tls = tiles[gtiles]
+if 'TARGET_NAME' in tiles.dtype.names:
+    selxmm = tiles['TARGET_NAME'] == 'XMM-LSS'
+    selcos = tiles['TARGET_NAME'] == 'COSMOS'
+    if args.target == 'medium':
+        sel = ~selxmm & ~selcos
+        tls = tls[sel]
+    if args.target == 'xmm':
+        tls = tls[selxmm]
+    if args.target == 'cosmos':
+        tls = tls[selcos]
 pad = 0.2
 if args.wficen != 'y':
     pad = 0.5
-tls = tiles[gtiles]
+
 logger.info('number of grism tiles in tiling file is '+str(len(tls)))
+
+if args.palist is not None:  # this will only work for medium targets
+    palist = np.array(args.palist).astype(float)
+    logger.info(
+        'palist provided, will set PA of repeated RA,DEC values to '+str(palist))
+    tlsu = unique(tls, keys=['RA', 'DEC'])
+    tral = []
+    tdecl = []
+    tpal = []
+    for i in range(0, len(tlsu)):
+        for pa in palist:
+            tpal.append(pa)
+            tral.append(tlsu['RA'][i]+args.radiff)
+            deci = tlsu['DEC'][i]+args.decdiff
+            if pa-palist[0] > 100:
+                deci += args.decpa
+            tdecl.append(deci)
+    tls = Table()
+    tls['RA'] = tral
+    tls['DEC'] = tdecl
+    tls['PA'] = tpal
+
 selreg = tls['RA'] > ram-2*pad/np.cos(args.decmin*np.pi/180)
 selreg &= tls['RA'] < rax+2*pad/np.cos(args.decmin*np.pi/180)
 selreg &= tls['DEC'] > decm-pad
@@ -290,7 +335,7 @@ for chunk in range(0, Nchunk):
                                 idx_det = ran_indices_tl[selp][i]
                                 idx.append(idx_det)
                                 # append the detector number to the list of bits for this point
-                                det_bits.append(int(2**det))
+                                det_bits.append(int(det))
         return idx, det_bits
 
     par = 'y'
@@ -320,15 +365,16 @@ for chunk in range(0, Nchunk):
         logger.info('input/final ids are matched in order')
     else:
         sys.exit('ids are not matched')
-    det_bits_u = np.zeros(rans.size)
-    np.add.at(det_bits_u, indices, det_bits_chunk)
+    # this doesn't work as a bitmask because some points are observed by the same detector in multiple tiles, so cannot reduce to a mapping of the set of detectors that see each point, would need to keep track of the number of times each detector sees each point to be able to encode as a bitmask, which is more complicated and may not be worth it for this application
+    # det_bits_u = np.zeros(rans.size)
+    # np.add.at(det_bits_u, indices, det_bits_chunk)
     racut = ral_tot[selobs]
     deccut = decl_tot[selobs]
     ra_all.append(racut)
     dec_all.append(deccut)
     cnts_all.append(cnts)
     indx_all.append(rans)
-    det_bits_all.append(det_bits_u)
+    # det_bits_all.append(det_bits_u)
 
     tf = time()
     logger.info('finished chunk '+str(chunk)+' in ' +
@@ -339,7 +385,7 @@ ra_all = np.concatenate(ra_all)
 dec_all = np.concatenate(dec_all)
 cnts_all = np.concatenate(cnts_all)
 indx_all = np.concatenate(indx_all)
-det_bits_all = np.concatenate(det_bits_all)
+# det_bits_all = np.concatenate(det_bits_all)
 logger.info('concatenated data '+str(len(ra_all)) +
             ' data points with at least one observation')
 
@@ -348,7 +394,7 @@ tout['RA'] = ra_all
 tout['DEC'] = dec_all
 tout['ID'] = indx_all
 tout['NOBS'] = np.array(cnts_all, dtype=int)
-tout['DET_BITS'] = np.array(det_bits_all, dtype=int)
+# tout['DET_BITS'] = np.array(det_bits_all, dtype=int)
 logger.info('about to write output to '+outf)
 tout.write(outf, overwrite=True)
 
